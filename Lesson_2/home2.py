94c79866bf3ac0f8fa584a79cee4598236d76235
pobud <pobudilin.vlad@gmail.com>
2026-07-04 00:33:58 +0300

    final

---
+++ Lesson_2/home2.py
@@ -0,0 +1,62 @@
+## 1. Квадрат числа.
+
+# square=int(input("Введите число:"))
+# print(square**2)
+
+###
+
+## 2. “Середнє трьох чисел”
+
+# num1=int(input("Введите первое чило:"))
+# num2=int(input("Введите второе число:"))
+# num3=int(input("Введите третье число:"))
+# arithmetic_mean=(num1+num2+num3)/3
+# print(f"Среднее арифметическое: {arithmetic_mean}")
+
+###
+
+## 3. “Перетворення хвилин у години”
+
+# minutes_e=int(input("Введите минуты:"))
+# hour=minutes_e//60
+# minutes=minutes_e%60
+# print(f"{hour}:{minutes}")
+
+###
+
+## 4. “Розрахунок знижки”
+
+# price=float(input("Введите цену на товар:"))
+# discount=int(input("Скидка (%):"))
+# discounted_price=price-price*discount/100
+# print(f"Цена со скидкой: {discounted_price}")
+
+###
+
+## 5. “Остання цифра числа”
+
+# number_1=int(input("Введите целое число:"))
+# number_last=number_1%10
+# print(f"Последняя цифра: {number_last}")
+
+###
+
+## 6. “Периметр прямокутника”
+
+# length=float(input("Введите длину:"))
+# width=float(input("Введите ширину:"))
+# perimeter=(length+width)*2
+# print(f"Периметр прмоугольника: {perimeter}")
+
+###
+
+## 7. Виведення числа в стовпчик
+
+# numb_4=int(input("Введите 4-х значное число:"))
+# numb1=numb_4//1000
+# numb02=numb_4//100
+# numb2=numb02%10
+# numb03=numb_4//10
+# numb3=numb03%10
+# numb4=numb_4%10
+# print(numb1, numb2, numb3, numb4, sep="\n")
