af35053d25cd631a31a48f9dd2aa319b78f1c149
pobud <pobudilin.vlad@gmail.com>
2026-07-08 23:36:51 +0300

    fix == 1

--- Lesson_3/home3.3.py
+++ Lesson_3/home3.3.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-if len(numbers) == 0 and len(numbers) == 1:
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+if len(numbers) == 0:
     numbers_coppy = numbers.copy()
     listcopy = [numbers, numbers_coppy]
     print(listcopy)
