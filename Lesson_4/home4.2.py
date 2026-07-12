78c4c382f77b4df527d9396b411682547c7e0f58
pobud <pobudilin.vlad@gmail.com>
2026-07-13 00:00:30 +0300

    final

---
+++ Lesson_4/home4.2.py
@@ -0,0 +1,15 @@
+numbers = [0, 1, 7, 2, 4, 8]
+totalsum = 0
+for even_numbers in range(0, len(numbers), 2):
+    totalsum += numbers[even_numbers]
+    total = totalsum * numbers[-1]
+print(total)
+
+###
+
+# numbers = [0, 1, 7, 2, 4, 8]
+# if len(numbers) == 0:
+#     total = 0
+# else:
+#     total = (sum(numbers[::2])*(numbers[-1]))
+# print(totalsum)
