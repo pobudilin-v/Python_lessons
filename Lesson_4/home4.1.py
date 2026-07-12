917146afb2ad94ee6c264f8c83e1ed324a81129c
pobud <pobudilin.vlad@gmail.com>
2026-07-12 22:49:42 +0300

    final

---
+++ Lesson_4/home4.1.py
@@ -0,0 +1,13 @@
+# numbers = [1, 0, 2, 3, 4, 0, 5, 0, 6, 0, 7, 8, 9, 0]
+# i = numbers.count(0)
+# while i:
+#     numbers.remove(0)
+#     numbers.append(0)
+#     i -= 1
+# print(numbers)
+
+###
+
+# numbers = [1, 0, 2, 3, 4, 0, 5, 0, 6, 0, 7, 8, 9, 0]
+# numbers.sort(key=lambda zero: zero == 0)
+# print(numbers)
